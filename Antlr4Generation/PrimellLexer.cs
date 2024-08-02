4f1e4ad5d84132bc3ad9546602724190763862c4
DPenner1 <dpenner1.online@gmail.com>
2024-08-02 14:35:59 -0400

    move grammar files up a level, outside the generated code folder

--- Antlr4Generation/PrimellLexer.cs
+++ Antlr4Generation/PrimellLexer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// Generated from /home/darrell/Projects/Primell/Antlr4Generation/PrimellLexer.g4 by ANTLR 4.13.1
+// Generated from /home/darrell/Projects/Primell/PrimellLexer.g4 by ANTLR 4.13.1
 
 // Unreachable code detected
 #pragma warning disable 0162
