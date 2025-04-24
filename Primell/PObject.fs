da482f6e2a22fdf31455632cf5857c8e5b829979
DPenner1 <dpenner1.online@gmail.com>
2025-04-23 22:29:58 -0400

    removed comment made obsolete by datatype restructure

--- Primell/PObject.fs
+++ Primell/PObject.fs
@@ -241,6 +241,9 @@ and PrimellValue =
   | Atom of PrimellAtom
   | Empty
 
+// references are a way to deal with indexing mutability (and is more of an implementation detail instead of a object within Primell)
+// so if you have a variable x, and index it at index i (x@i), the parser simply generates a reference with values x and i, 
+// figuring assignments out based on that later. References can also reference other references [citation needed]
 and Reference =  // TODO - naming, not sure i like type and one of the options being named the same
   | Variable of string
   | Reference of ReferencedObject: PrimellObject * ReferenceIndex: PrimellObject
@@ -322,22 +325,6 @@ and PrimellObject (value: PrimellValue, ?metadata: PrimellMetaData) =
 
   member this.WithReference (ref: Reference) =
     PrimellObject(this.Value, {this.Metadata with Reference = ref})
-    
-  // I really don't like this one: In mutable paradigm, this would just be implemented right here.
-  // But in immutable, it effectively requires a copy constructor to get the object with a reference
-  // and in principle, without having reference to downstream types, this forces them
-  // to implement what is effectively a mechanical parsing detail
-  (* abstract member WithReference: Reference -> PrimellObject
-
-  member val Reference = defaultArg refersTo Void with get
-
-  abstract member NaNAwareEquals: PrimellObject -> bool
-  default this.NaNAwareEquals pobj = this.Equals(pobj)
-  *)
-
-// references are a way to deal with indexing mutability (and is more of an implementation detail instead of a object within Primell)
-// so if you have a variable x, and index it at index i (x@i), the parser simply generates a reference with values x and i, 
-// figuring assignments out based on that later. References can also reference other references [citation needed]
 
 and PObject = PrimellObject
 
